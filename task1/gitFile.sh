b672f1ef98d97abb835c0b3c1333e4a703f25342
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-11 21:59:07 +0600

    Задание 1

---
+++ task1/gitFile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# muradil
+
+mkdir directory1
+cd directory1
+mkdir subdirectory1
+cd ..
+mkdir directory2
+cd directory2
+touch hello.txt
+echo "Hello from JS" > hello.txt
+cd ..
+mkdir directory3
+cd directory3
+mkdir suddirectory2
